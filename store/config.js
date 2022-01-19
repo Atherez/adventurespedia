9de5f32fb4779cc214926cdc8887226e7f60cda9
Aryan Gupta <webworksdesigners@gmail.com>
2022-01-19 20:46:29 +0530

    Update config.js

--- store/config.js
+++ store/config.js
@@ -2,6 +2,6 @@
 export const state= ()  => ({
   site_logo:'https://blog.travelpayouts.com/en/wp-content/themes/theme/images/header-logo.svg',
   site_title: `Swati's Blog`,
-  site_url: `localhost:3000`,
-  wp_url:'http://3.145.107.4/index.php',
+  site_url: `https://adventurespedia.com`,
+  wp_url:'https://blog.adventurespedia.com',
 })
