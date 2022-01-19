b8c101154caa24fd1790a8bf46f677bf631e8313
Aryan Gupta <webworksdesigners@gmail.com>
2022-01-20 00:16:51 +0530

    removed amplify

--- nuxt.config.js
+++ nuxt.config.js
@@ -100,7 +100,6 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/seo',
-    { src: '~/plugins/amplify.js', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
