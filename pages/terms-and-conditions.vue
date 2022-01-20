b7b38426952ecce980981297b422cb1edcabab78
Aryan Gupta <webworksdesigners@gmail.com>
2022-01-20 21:00:54 +0530

    Terms & Conditions

--- pages/terms-and-conditions.vue
+++ pages/terms-and-conditions.vue
@@ -1,5 +1,96 @@
 <template>
-    <div>
-        T and C
-    </div>
+    <v-container>
+      <v-row>
+        <v-col>
+          <h1 class="text-center py-2"><u>Terms {{'&'}} Conditions</u></h1>
+          <p>
+          Welcome to adventurespedia.com (hereafter referred as "AP"). By accessing and using AP and their services (the “Service”) you are agreeing to be legally bound by these Terms of Use.
+          </p>
+
+          <h2><u>1. GENERAL</u></h2>
+          <p>
+          AP may change these Terms of Use from time to time so be sure to check back periodically. Your use of the AP after any changes have been posted will constitute your agreement to the modified Terms of Use. You understand and agree that AP may discontinue or change the AP and/or the Service at any time, without notice. You also understand and agree that AP may discontinue or restrict your use of the AP for any reason without notice and without any liability to you. In addition, when using the AP, you shall be subject to any posted guidelines or rules applicable to the Service, which may be posted from time to time and which are hereby incorporated by reference into these Terms of Use.
+          </p>
+
+          <h2><u>2. COPYRIGHT</u></h2>
+          <p>
+          The content displayed on the AP, including the selection, arrangement, and design (“Content”) is the property of AP or its licensors, and is protected by copyright and other intellectual property laws. The Content may be used only for your personal and non-commercial use and may not be edited or modified for any purpose. By accessing the AP, you agree not to reproduce, retransmit, distribute, disseminate, display, sell, publish, broadcast or circulate the Content to anyone, except that you may occasionally reproduce, distribute, display or transmit an insubstantial portion of Content, for a noncommercial purpose, to a limited number of individuals, provided you use the phrase “Used with permission from the AP.” All rights not expressly granted herein are hereby reserved.
+          </p>
+          <p>
+          AP does not claim ownership of the Content you submit or make available for inclusion on the AP. The Content is the property of the author of such Content. You agree to grant AP a perpetual, royalty-free and irrevocable right and license to use, reproduce, modify, adapt, publish, translate, distribute, transmit, publicly display, publicly perform, sublicense, create derivative works from, transfer, and sell such Content in any format now know or hereafter created, and to use your name and other identifying information you provide in connection with that Content. You also permit any visitor or member of the AP to use such Content for personal use as described above.
+          </p>
+          <p>
+          If you believe in good faith that any Content infringes your copyright, you may send us a notice requesting that the material be removed though we cannot guarantee that any action will be taken as a result of your correspondence. Notices should be sent here.
+          </p>
+
+          <h2><u>3. DISCLAIMER OF WARRANTIES AND LIABILITY</u></h2>
+          <p>
+          You agree that your access to, and use of, the Service and the Content available on the AP is on an “as-is”, “as available” basis. The AP include facts, views, opinions and recommendations of individuals and organizations deemed of interest, and AP does not endorse these views, opinions and recommendations. AP specifically disclaims any representations or warranties, express or implied, as to the AP and the Content, including, without limitation, any representations or warranties of accuracy, completeness, timeliness, noninfringement, merchantability or fitness for a particular purpose. AP will not be liable to you or anyone else for any loss or injury caused in whole or part by its negligence or contingencies beyond its control in procuring, compiling, interpreting, reporting or delivering the AP and the Content. In no event will AP be liable to you or anyone else for any decision made or action taken by you in reliance on the Content. AP shall not be liable to you or anyone else for any damages (including, without limitation, consequential, special, incidental, indirect, or similar damages), other than direct damages, even if advised of the possibility of such damages. Because some states or jurisdictions do not allow the exclusion or limitation of liability for damages or the exclusion of certain types of warranties, parts or all of the above limitation may not apply to you.
+          </p>
+
+          <h2><u>4. LINKS TO THIRD PARTY SITESY</u></h2>
+          <p>
+          The AP may contain hyperlinks to web sites operated by persons other than AP. You agree not to hold AP responsible for the content or operation of such web sites. A hyperlink from the AP to another web site does not imply or mean that AP endorses the content on that web site or the operator or operations of that site. You are solely responsible for determining the extent to which you may use any content at any other web sites to which you might link from the AP.
+          </p>
+
+          <h2><u>5. ADDITIONAL LEGAL TERMS</u></h2>
+          <p>
+          These Terms of Use, your rights and obligations, and all actions contemplated by these Terms of Use will be governed by the laws of the Republic of India, as if these Terms of Use were a contract wholly entered into and wholly performed within the Republic of India. If any provision in these Terms of Use is invalid or unenforceable under applicable law, the remaining provisions will continue in full force and effect, and the invalid or unenforceable provision will be deemed superseded by a valid, enforceable provision that most closely matches the intent of the original provision. These Terms of Use are the entire and final agreement regarding the AP and their Content, and supersede any prior or contemporaneous communications between AP and you regarding the AP and their Content.
+          </p>
+
+
+          <h2><u>6. USER OBLIGATIONS</u></h2>
+          <p>
+          In consideration of your use of the Service, you represent that you are of legal age to form a binding contract and are not a person barred from receiving services under the laws of the Republic of India or other applicable jurisdiction. You agree to provide true, accurate, current and complete information about yourself upon registration and thereafter. You are responsible for maintaining the confidentiality of your password and account, and are fully responsible for all activities that occur under your password or account. You agree to immediately notify the AP of any unauthorized use of your password or account or any other breach of security. AP cannot and will not be liable for any loss or damage arising from your failure to comply this Section 6. You agree to comply with all local rules regarding online conduct and acceptable Content, including all applicable laws regarding the transmission of technical data exported from the country in which you reside.
+          </p>
+          <p>
+          You agree not to use the AP or the Service to:
+          </p>
+              <ul>
+              <li>
+              Upload, post, email, transmit or otherwise make available (“Post”) any Content:
+                  that is unlawful, harmful, threatening, abusive, harassing, tortious, defamatory, vulgar, obscene, libelous, false or inaccurate, invasive of another’s privacy, hateful, or racially, ethnically or otherwise objectionable;
+                  that you do not have a right to make available under any law or under contractual or fiduciary relationships (such as inside information, proprietary and confidential information learned or disclosed as part of employment relationships or under nondisclosure agreements); or
+                  that infringes any patent, trademark, trade secret, copyright or other proprietary rights (“Rights”) of any party;
+              harm minors in any way;
+              </li>
+              <li>
+                Impersonate any person or entity, or falsely state or otherwise misrepresent your affiliation with a person or entity;
+              </li>
+              <li>
+                Post any unsolicited or unauthorized advertising, promotional materials, “junk mail,” “spam,” “chain letters,” “pyramid schemes,” or any other form of solicitation;
+              </li>
+              <li>
+                Post any material that contains software viruses or any other computer code, files or programs designed to interrupt, destroy or limit the functionality of any computer software or hardware or telecommunications equipment;
+              </li>
+              <li>
+                Interfere with or disrupt the Service or servers or networks connected to the Service, or disobey any requirements, procedures, policies or regulations of networks connected to the Service;
+              </li>
+              <li>
+                Violate any applicable local, state, national or international law, intentionally or unintentionally;
+              </li>
+              <li>
+                “stalk” or otherwise harass another; and/or
+              </li>
+              <li>
+                Collect or store personal data about other users.
+              </li>
+            </ul>
+          <p>
+          You agree to be fully responsible for your own Content. The AP act as a venue for you to Post your Content. While AP has the right to refuse to post or remove, edit or abridge any Content for any reason, it is otherwise not involved in the writing or decisions as to the Content of its users. As a result, AP has no control over the quality, accuracy or legality of the Content on the AP. All Content posted on the AP is the sole responsibility of the person from whom such Content originated. Because the AP are venues, in the event that you have a dispute with a user of the AP and their Service, you release AP and its officers, members and employees from claims, demands and damages (actual and consequential) of every kind and nature, known and unknown, suspected and unsuspected, disclosed and undisclosed, arising out of or in any way connected with such disputes. You agree to indemnify and hold AP and its officers, members and employees harmless from any claims, damages, losses or costs (including reasonable attorney’s fees) that arise out of any of your Content, including, without limitation, any Content posted or otherwise provided by you that infringes any copyright, trademark, intellectual property right of any person or defames any person or violates any person’s rights of publicity or privacy, or otherwise violates these Terms of Use.
+          </p>
+
+
+          <h2><u>7. PRIVACY AND USE OF INFORMATION.</u></h2>
+          <p>
+          Please visit our Privacy Policy, which also governs your use of the AP, to understand how we collect and use information about you. If you are outside the Republic of India, you understand and agree that by providing the AP any information about yourself, you are consenting to the transfer of that information to the Republic of India.
+          </p>
+
+          <h2><u>8. TRADEMARK INFORMATION</u></h2>
+          <p>
+          The AP logo, Adventures Pedia Blog, AP, and the other trademarks, service marks, logos and trade names displayed on the AP are the trademarks of AP. You agree not to display or use the AP Marks in any manner without AP’s prior written consent.
+          </p>
+        </v-col>
+      </v-row>
+    </v-container>
 </template>
