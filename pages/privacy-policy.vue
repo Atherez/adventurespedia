9b356a92495911a746bd43b5d4c720add8137807
Aryan Gupta <webworksdesigners@gmail.com>
2022-01-20 20:42:45 +0530

    Privacy Policy

--- pages/privacy-policy.vue
+++ pages/privacy-policy.vue
@@ -1,5 +1,55 @@
 <template>
-    <div>
-        Privacy Policy
-    </div>
+    <v-container class="pt-2">
+      <v-row>
+        <v-col>
+          <h1 class="text-center py-2"><u>Privacy Policy</u></h1>
+          <h2><u>GENERAL</u></h2>
+          <p>
+          We at adventurespedia.com (hereafter referred to as “AP”) are committed to maintaining the privacy of personal information that you provide to us when using AP (hereafter referred to collectively as the “Services”). This Privacy Policy describes how we treat personal information received about you when you visit AP.
+          </p>
+
+          <h2><u>INFORMATION WE COLLECT</u></h2>
+          <p>
+          We may collect personally identifiable information from you that may include your name, age, address, e-mail address, and other information. We do not, however, knowingly collect personal information from children under the age of thirteen. In addition, we may conduct occasional online surveys. These surveys are entirely voluntary and you may easily decline to participate. Furthermore, we automatically collect non- personally identifiable IP addresses and other Web site usage information from you when you visit AP.
+          </p>
+
+          <h2><u>HOW WE USE INFORMATION COLLECTED</u></h2>
+          <p>
+          We may use information: (i) for the purposes for which you specifically provided the information, (ii) to send you e-mails on our behalf and on behalf of other relevant parties unless you have opted out of receiving such e-mail, (iii) to enhance existing features or develop new features, products and services, and (iv) to provide advertisers and other third parties with aggregate (non-personally identifiable) information about our user base and usage patterns.
+          </p>
+          <p>
+          We will not share your personally identifiable information outside of AP Media unless you “opt in” to having your personally identifiable information shared with a company that is not affiliated with us. However, we may disclose and use personally identifiable information in special circumstances where it is necessary to enforce our Terms of Use or when we, in good faith, believe that the law requires us to do so.
+          </p>
+          <p>
+          Notwithstanding the forgoing, if we collect your personally identifiable information in connection with our relationships with third parties (i.e., on behalf of third parties affiliated with political or other advocacy groups): (i) we may share with third parties any information you provide, or that is collected, in the course of visiting any pages that are made available in cooperation with such third parties, and (ii) if you provide an e-mail address in this context, we reserve the right to present you with advertisements on behalf of third parties.
+          </p>
+
+
+          <h2><u>PARTICIPATION, DISCUSSION AND COMMENTS</u></h2>
+          <p>
+          Certain participation, discussion and comments functions on AP allow you to post your own opinions and points of view and to comment on AP posts and on what other readers have to say. AP reserves the right to use such user-generated content, as well as any content provided to AP via e-mail, for any purpose. When you use these features (for example, by participating in a discussion or making a comment), you may be making public any information that you provide including your name, location and e- mail address. AP is not responsible for any personal information that you make public via the Services’ features, and has no obligation to remove any content that you made available thereby. Furthermore, AP is not responsible for the actions or disclosures of any users of AP Services.
+          </p>
+
+          <h2><u>COOKIES AND WEB BEACONS</u></h2>
+          <p>
+          We use cookies to allow you to move quickly through our site, to recognize repeat visitors and to allow you to take advantage of some of our site’s special features. A cookie is a string of information that is sent by a Web site and stored on your hard drive or temporarily in your computer’s memory. Cookies are small files (just a few hundred bytes) and won’t pose any significant disk-space concerns. You may also receive cookies and web beacons from our third party network advertisers. Web beacons are invisible images used to recognize users and assess traffic patterns; they do not use your name, email address or any other personally identifiable information. This privacy statement covers the use of cookies by AP Media only and does not cover the use of cookies or web beacons by any third-parties.
+          </p>
+
+          <h2><u>LINKS</u></h2>
+          <p>
+          The Services may contain links to other sites. Please be aware that AP Media is not responsible for the privacy practices of such other sites. We encourage our users to be aware when they leave our site and to read the privacy statements of each and every Web site that collects personally identifiable information.
+          </p>
+
+          <h2><u>TRANSFER TO U.S. OR OTHER COUNTRIES</u></h2>
+          <p>
+          Your information may be stored and processed in the United States or any other country where AP has facilities, and by subscribing to our services, you consent to the transfer of information outside of your country.
+          </p>
+
+          <h2><u>AMENDMENTS</u></h2>
+          <p>
+          AP may choose to amend this policy periodically. Any changes will be available for review on this page.
+          </p>
+        </v-col>
+      </v-row>
+    </v-container>
 </template>
