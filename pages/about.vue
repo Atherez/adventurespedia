2490c9e679aaa4fe2db4c184de49a4d5dcea3dbf
Aryan Gupta <webworksdesigners@gmail.com>
2022-01-23 01:12:31 +0530

    About page started

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,30 @@
 <template>
-    <div>
-        ABOUT
-    </div>
+  <div class="about-page pa-4">
+    <v-container class="py-4 px-0 px-lg-10">
+      <client-only>
+      <v-row ustify="center" class="pb-6" justify="center">
+        <v-col cols="12" class="text-center text-h5 pb-0 primary--text font-weight-bold">
+          About Us
+        </v-col>
+         <v-col cols="12" class="text-center text-body-1">
+           We, at AdventuresPedia, are focused to bring you accurate and relevant information related to <br />
+           <span class="font-weight-bold">
+             Fights, Hotels, Travel, Visa and Passport.
+           </span>
+        </v-col>
+        <v-col cols="12" class="text-center text-h5 pb-0 primary--text font-weight-bold">
+          Affiliate Disclosure
+        </v-col>
+         <v-col cols="12" class="text-center text-body-2">
+           AdventuresPedia is a participant in the Amazon Services LLC Associates Program, an affiliate advertising <br/>
+           program designed to provide a means for sites to earn advertising fees by advertising and linking to <br/>
+           amazon.com, amazon.ca, amazon.co.uk or amazon.in.
+        </v-col>
+      </v-row>
+      </client-only>
+    </v-container>
+
+  </div>
 </template>
+
+
