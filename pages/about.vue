5b5938817e26331d1d46565815c2b2411697a5f3
Aryan Gupta <webworksdesigners@gmail.com>
2022-01-26 03:40:55 +0530

    About page done

--- pages/about.vue
+++ pages/about.vue
@@ -6,16 +6,16 @@
         <v-col cols="12" class="text-center text-h5 pb-0 primary--text font-weight-bold">
           About Us
         </v-col>
-         <v-col cols="12" class="text-center text-body-1">
-           We, at AdventuresPedia, are focused to bring you accurate and relevant information related to <br />
-           <span class="font-weight-bold">
-             Fights, Hotels, Travel, Visa and Passport.
-           </span>
+         <v-col cols="12" md="8" class="text-center">
+           AdventuresPedia is a thoughtfully-created informative website for all things traveling - from <span class="primary--text">visas, passports, and flights to looming volcanoes and scenic beaches</span> - we cover it all. As your ‘virtual’ backpacking companions, we, at AdventuresPedia, aim to bring you meticulously accurate and relevant content on hotels, flights, city recommendations, and travel FAQs and inspirations.
+            <br/>
+            <br />
+We are a team of adrenaline-seeking adventure enthusiasts with a passion for blogging and sharing our knowledge with the world. Our team is devoted to constantly foraging for brain-racking questions related to travel and providing the most accurate and relevant information on the same, to our dedicated readers and visitors.
         </v-col>
         <v-col cols="12" class="text-center text-h5 pb-0 primary--text font-weight-bold">
           Affiliate Disclosure
         </v-col>
-         <v-col cols="12" class="text-center text-body-2">
+         <v-col cols="12" md="8" class="text-center text-body-2">
            AdventuresPedia is a participant in the Amazon Services LLC Associates Program, an affiliate advertising <br/>
            program designed to provide a means for sites to earn advertising fees by advertising and linking to <br/>
            amazon.com, amazon.ca, amazon.co.uk or amazon.in.
